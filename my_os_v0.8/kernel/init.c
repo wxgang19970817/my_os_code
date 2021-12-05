0c54a38e8097d063049e4cf22fcc8c1f79ba3604
XG_W <wxgang19970817@163.com>
2021-12-05 20:24:15 +0800

    v0.8.2

--- my_os_v0.8/kernel/init.c
+++ my_os_v0.8/kernel/init.c
@@ -5,12 +5,15 @@
 	> Created Time: 2021年12月02日 星期四 19时19分44秒
  ************************************************************************/
 #include "init.h"
-#include "print.h"
+#include "../lib/kernel/print.h"
 #include "interrupt.h"
+#include "../device/timer.h"
+
 
 /* 负责初始化所有模块 */
 void init_all()
 {
     put_str("init_all\n");
-    idt_init();
+    idt_init();             //初始化中断
+    timer_init();           //初始化PIT
 }
