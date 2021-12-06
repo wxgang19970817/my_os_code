128c2dadb29eb770698d90cc5443c46f812c6d9f
XG_W <wxgang19970817@163.com>
2021-12-06 21:35:07 +0800

    v0.9

---
+++ my_os_v0.9/kernel/main.c
@@ -0,0 +1,12 @@
+#include "../lib/kernel/print.h"
+#include "init.h"
+#include "debug.h"
+
+int main(void)
+{
+    put_str("I am kernel\n");
+    init_all();
+    ASSERT(1==2);
+    while(1);
+    return 0;
+}
