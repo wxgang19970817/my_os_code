b58a92c4a68cbf356290461a73d53252cd8ed5b1
XG_W <wxgang19970817@163.com>
2022-01-04 10:32:12 +0800

    v1.6.4

--- my_os_v1.6/kernel/main.c
+++ my_os_v1.6/kernel/main.c
@@ -10,7 +10,7 @@
 #include "stdio.h"
 #include "memory.h"
 #include "fs.h"
-
+#include "string.h"
 void k_thread_a(void*);
 void k_thread_b(void*);
 void u_prog_a(void);
@@ -30,10 +30,28 @@ int main(void)
     thread_start("k_thread_b", 31, k_thread_b, "I am thread_b ");
 
     uint32_t fd = sys_open("/file1",O_RDWR);
-    printf("fd:%d\n",fd);
-    sys_write(fd,"hello,world\n",12);
+    printf("open /file1,fd:%d\n",fd);
+    char buf[64] = {0};
+    int read_bytes = sys_read(fd,buf,18);
+    printf("1_read %d bytes:\n%s\n",read_bytes,buf);
+
+    memset(buf,0,64);
+    read_bytes = sys_read(fd,buf,6);
+    printf("2_read %d bytes:\n%s",read_bytes,buf);
+
+    memset(buf,0,64);
+    read_bytes = sys_read(fd,buf,6);
+    printf("3_read %d bytes:\n%s",read_bytes,buf);
+
+
+    printf("______ close file 1 and reopen ________________\n");
     sys_close(fd);
-    printf("%d closed now\n",fd);
+    fd = sys_open("/file1",O_RDWR);
+    memset(buf,0,64);
+    read_bytes = sys_read(fd,buf,24);
+    printf("4_read %d bytes:\n%s",read_bytes,buf);
+
+    sys_close(fd);
     while(1);
     return 0;
 }
