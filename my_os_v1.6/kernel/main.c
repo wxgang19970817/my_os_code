2d0866f46f18211c7cc7ca8b2b5308761643e29b
XG_W <wxgang19970817@163.com>
2022-01-05 11:30:32 +0800

    v1.6.8

--- my_os_v1.6/kernel/main.c
+++ my_os_v1.6/kernel/main.c
@@ -24,37 +24,51 @@ int main(void)
     init_all();
  
 
-    struct dir* p_dir  = sys_opendir("/dir1/subdir1");
-    if(p_dir)
-    {
-        printf("/dir1/subdir1 open done!\n content:\n");
-        char* type = NULL;
-        struct dir_entry* dir_e = NULL;
-        while((dir_e = sys_readdir(p_dir)))
-        {
-            if(dir_e->f_type == FT_REGULAR)
-            {
-                type = "regular";
-            }
-            else
-            {
-                type = "directory";
-            }
-            printf("   %s    %s\n",type,dir_e->filename);
-        }
-        if(sys_closedir(p_dir) == 0)
-        {
-            printf("/dir1/subdir1 close done!\n");
-        }
-        else
-        {
-            printf("/dir1/subdir1 close fail!\n");
-        }
-    }
-    else
-    {
-        printf("/dir1/subdir1 open failed!\n");
-    }
+    printf("try to delete nonempty directory /dir1/subdir1\n");
+    struct dir* dir = sys_opendir("/dir1/");
+    char* type = NULL;
+    struct dir_entry* dir_e = NULL;
+    while((dir_e = sys_readdir(dir))) { 
+    if (dir_e->f_type == FT_REGULAR) {
+	    type = "regular";
+      } else {
+	 type = "directory";
+      }
+      printf("      %s   %s\n", type, dir_e->filename);
+   }
+   printf("try to delete nonempty directory /dir1/subdir1\n");
+   if (sys_rmdir("/dir1/subdir1") == -1) 
+   {
+      printf("sys_rmdir: /dir1/subdir1 delete fail!\n");
+   }
+
+   printf("try to delete /dir1/subdir1/file2\n");
+   if (sys_rmdir("/dir1/subdir1/file2") == -1) 
+   {
+      printf("sys_rmdir: /dir1/subdir1/file2 delete fail!\n");
+   } 
+
+   if (sys_unlink("/dir1/subdir1/file2") == 0 ) 
+   {
+      printf("sys_unlink: /dir1/subdir1/file2 delete done\n");
+   }
+   
+   printf("try to delete directory /dir1/subdir1 again\n");
+   if (sys_rmdir("/dir1/subdir1") == 0) 
+   {
+      printf("/dir1/subdir1 delete done!\n");
+   }
+
+   printf("/dir1 content after delete /dir1/subdir1:\n");
+   sys_rewinddir(dir);
+   while((dir_e = sys_readdir(dir))) { 
+      if (dir_e->f_type == FT_REGULAR) {
+	 type = "regular";
+      } else {
+	 type = "directory";
+      }
+      printf("      %s   %s\n", type, dir_e->filename);
+   }
     while(1);
     return 0;
 }
