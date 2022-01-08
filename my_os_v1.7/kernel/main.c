d463840e9864ab73317902e0813156adb395b6c4
XG_W <wxgang19970817@163.com>
2022-01-08 12:00:10 +0800

    v1.7.5

--- my_os_v1.7/kernel/main.c
+++ my_os_v1.7/kernel/main.c
@@ -24,20 +24,20 @@ int main(void)
     init_all();
     
     /******** 写入应用程序 ***********/
-    // uint32_t file_size = 4777;
-    // uint32_t sec_cnt = DIV_ROUND_UP(file_size,512);
-    // struct disk* sda = &channels[0].devices[0];
-    // void* prog_buf = sys_malloc(file_size);
-    // ide_read(sda,300,prog_buf,sec_cnt);
-    // int32_t fd = sys_open("/prog_no_arg",O_CREAT | O_RDWR);
-    // if(fd != 1)
-    // {
-    //     if(sys_write(fd,prog_buf,file_size) == -1)
-    //     {
-    //         printk("file write error! \n");
-    //         while(1);
-    //     }
-    // }
+    uint32_t file_size = 4777;
+    uint32_t sec_cnt = DIV_ROUND_UP(file_size,512);
+    struct disk* sda = &channels[0].devices[0];
+    void* prog_buf = sys_malloc(file_size);
+    ide_read(sda,300,prog_buf,sec_cnt);
+    int32_t fd = sys_open("/prog_arg",O_CREAT | O_RDWR);
+    if(fd != 1)
+    {
+        if(sys_write(fd,prog_buf,file_size) == -1)
+        {
+            printk("file write error! \n");
+            while(1);
+        }
+    }
 
     cls_screen();
     console_put_str("[rabbit@localhost /]$");
