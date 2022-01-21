c9d5e56e656dc0138a5e51c6bdef3be5387ba51e
XG_W <wxgang19970817@163.com>
2022-01-21 15:09:26 +0800

    note

---
+++ code/my_os_v1.0/lib/kernel/print.h
@@ -0,0 +1,21 @@
+/*************************************************************************
+	> File Name: print.h
+	> Author: wxgang
+	> Mail: wxgang19970817@163.com 
+	> Created Time: 2021年11月27日 星期六 19时15分40秒
+ ************************************************************************/
+
+#ifndef _LIB_KERNEL_PRINT_H
+#define _LIB_KERNEL_PRINT_H
+
+#include "../stdint.h"
+
+void put_char(uint8_t char_asci);
+void put_str(char * message);
+void put_int(uint32_t num );
+
+
+
+
+
+#endif
